ebe53c98d156bd53e31bcfc14656237f5061717b
jiahuigeng <jiahui.geng@rwth-aachen.de>
2022-07-13 16:00:36 +0200

    debug baseline

--- experiments/realworld/trainer_baseline_dev.py
+++ experiments/realworld/trainer_baseline_dev.py
@@ -1,191 +1,164 @@
-import argparse
-import json
-import logging
-from collections import OrderedDict, defaultdict
-from pathlib import Path
-from time import time, ctime
+import os
+import os.path as osp
 import numpy as np
+import pandas as pd
+
+from PIL import Image
+
 import torch
-import torch.utils.data
-from tqdm import trange
-import copy
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import resnet
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
 
 
-from experiments.realworld.clients import RealClients
-# from experiments.backbone1 import ResNet
-from experiments.backbone import get_feature_extractor
-from utils import get_device, set_logger, set_seed, detach_to_numpy, save_experiment, \
-    print_calibration, calibration_search, offset_client_classes, calc_metrics
+def fix_all_seeds(seed):
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
 
-from experiments.calibrate import ECELoss
 
-parser = argparse.ArgumentParser(description="Personalized Federated Learning")
+fix_all_seeds(2021)
 
-#############################
-#       Dataset Args        #
-#############################
-# parser.add_argument(
-#     "--data-name", type=str, default="cifar10",
-#     choices=['cifar10', 'cifar100', 'cinic10', 'panda', 'sicapv2', "minipanda"],
-# )
-parser.add_argument('-n', '--data-name', default=['sicapv2', 'radboud', 'karolinska'], nargs='+')
-parser.add_argument("--data-path", type=str, default="../datafolder", help="dir path for CIFAR datafolder")
-# parser.add_argument("--num-clients", type=int, default=50, help="number of simulated clients")
-parser.add_argument("--model", type=str, default="resnet18", choices=["resnet18", "resnet50", 'efficientnetb3', 'efficientnetb5'])
-parser.add_argument("--ft", '-ft', default='cnn',
-                    choices=['cnn', 'resnet18', 'resnet50', 'efficientnetb3', 'efficientnetb5'])
 
-##################################
-#       Optimization args        #
-##################################
-parser.add_argument("--num-steps", type=int, default=1000)
-parser.add_argument("--optimizer", type=str, default='sgd', choices=['adam', 'sgd'], help="learning rate")
-parser.add_argument("--batch-size", type=int, default=512)
-parser.add_argument("--inner-steps", type=int, default=1, help="number of inner steps")
-parser.add_argument("--num-client-agg", type=int, default=1, help="number of kernels")
+class Sicapv2Dataset(Dataset):
+    def __init__(self, df):
+        # self.csv = csv.reset_index(drop=True)
+        self.data_df = df
+        self.path = SICAPV2_PATH
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        onehot_targets = self.data_df[['NC', 'G3', 'G4', 'G5']].values
+        self.targets = np.argmax(onehot_targets, axis=1)
 
-parser.add_argument("--lr", type=float, default=5e-2, help="learning rate")
-parser.add_argument("--wd", type=float, default=1e-3, help="weight decay")
-parser.add_argument("--n-kernels", type=int, default=16, help="number of kernels")
+    def __len__(self):
+        return self.data_df.shape[0]
 
-################################
-#       GP args        #
-################################
-parser.add_argument('--embed-dim', type=int, default=84)
-parser.add_argument('--loss-scaler', default=1., type=float, help='multiplicative element to the loss function')
-parser.add_argument('--kernel-function', type=str, default='RBFKernel',
-                    choices=['RBFKernel', 'LinearKernel', 'MaternKernel'],
-                    help='kernel function')
-parser.add_argument('--objective', type=str, default='predictive_likelihood',
-                    choices=['predictive_likelihood', 'marginal_likelihood'])
-parser.add_argument('--predict-ratio', type=float, default=0.5,
-                    help='ratio of samples to make predictions for when using predictive_likelihood objective')
-parser.add_argument('--num-gibbs-steps-train', type=int, default=5, help='number of sampling iterations')
-parser.add_argument('--num-gibbs-draws-train', type=int, default=20, help='number of parallel gibbs chains')
-parser.add_argument('--num-gibbs-steps-test', type=int, default=5, help='number of sampling iterations')
-parser.add_argument('--num-gibbs-draws-test', type=int, default=30, help='number of parallel gibbs chains')
-parser.add_argument('--outputscale', type=float, default=8., help='output scale')
-parser.add_argument('--lengthscale', type=float, default=1., help='length scale')
-parser.add_argument('--outputscale-increase', type=str, default='constant',
-                    choices=['constant', 'increase', 'decrease'],
-                    help='output scale increase/decrease/constant along tree')
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+        row = self.data_df.iloc[index]
 
-#############################
-#       General args        #
-#############################
-parser.add_argument('--color', type=str, default='darkblue', help='color for calibration plot')
-parser.add_argument("--gpus", type=str, default='0', help="gpu device ID")
-parser.add_argument("--exp-name", type=str, default='', help="suffix for exp name")
-parser.add_argument("--eval-every", type=int, default=25, help="eval every X selected steps")
-parser.add_argument("--save-path", type=str, default="./output/pFedGP", help="dir path for output file")
-parser.add_argument("--seed", type=int, default=42, help="seed value")
-parser.add_argument("--input-size", type=int, default=32, help="input size")
-parser.add_argument("--mini", type=bool, default=False, help="use mini size")
-parser.add_argument("--classes-per-client", type=int, help="classes per client")
+        image = Image.open(osp.join(self.path, "images", row["image_name"]))
+        if self.transform is not None:
+            image = self.transform(image)
 
-args = parser.parse_args()
+        target = self.targets[index]
+        return image, target
 
-set_logger()
-set_seed(args.seed)
+
+SICAPV2_PATH = "/data/BasesDeDatos/SICAP/SICAPv2/"
+
+train_df_raw = pd.read_excel(osp.join(SICAPV2_PATH, "partition/Validation/Val1", "Train.xlsx"))
+val_df_raw = pd.read_excel(osp.join(SICAPV2_PATH, "partition/Validation/Val1", "Test.xlsx"))
+test_df_raw = pd.read_excel(osp.join(SICAPV2_PATH, "partition/Test", "Test.xlsx"))
+
+train_set = Sicapv2Dataset(train_df_raw)
+val_set = Sicapv2Dataset(val_df_raw)
+test_set = Sicapv2Dataset(test_df_raw)
+
+train_loader = DataLoader(train_set, batch_size=16, shuffle=True)
+val_loader = DataLoader(train_set, batch_size=16, shuffle=False)
+test_loader = DataLoader(train_set, batch_size=16, shuffle=False)
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_channel=3, num_class=4, pretrained=True, model='resnet18'):
+        super(ResNet, self).__init__()
+        self.num_channel = num_channel
+        self.num_class = num_class
+        self.model = model
+        self.pretrained = pretrained
+        if self.model == 'resnet18':
+            base = resnet.resnet18(pretrained=self.pretrained)
+            self.resnet_expansion = 1
+            print("ResNet18 is used.")
+        elif self.model == 'resnet34':
+            base = resnet.resnet34(pretrained=self.pretrained)
+            self.resnet_expansion = 1
+            print("ResNet34 is used.")
+        elif self.model == 'resnet50':
+            base = resnet.resnet50(pretrained=self.pretrained)
+            self.resnet_expansion = 4
+            print("ResNet50 is used.")
+        elif self.model == 'resnet101':
+            base = resnet.resnet101(pretrained=self.pretrained)
+            self.resnet_expansion = 4
+            print("ResNet101 is used.")
+        elif self.model == 'resnet152':
+            base = resnet.resnet152(pretrained=self.pretrained)
+            self.resnet_expansion = 4
+            print("ResNet152 is used.")
+        else:
+            raise NotImplemented('Requested model is not supported.')
+
+        self.in_block = nn.Sequential(
+            nn.Conv2d(self.num_channel, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
+            base.bn1,
+            base.relu,
+            base.maxpool)
+        self.encoder1 = base.layer1
+        self.encoder2 = base.layer2
+        self.encoder3 = base.layer3
+        self.encoder4 = base.layer4
+        self.avgpool = base.avgpool
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(512 * self.resnet_expansion, self.num_class, bias=True)
+
+    def forward(self, x):
+        h = self.in_block(x)
+        h = self.encoder1(h)
+        h = self.encoder2(h)
+        h = self.encoder3(h)
+        h = self.encoder4(h)
+        y = self.fc(self.flatten(self.avgpool(h)))
+        return y
+
+
+net = ResNet()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 cuda = torch.cuda.is_available()
-device = get_device(cuda=int(args.gpus) >= 0, gpus=args.gpus)
-print("device:", device)
 
-
-num_classes = {
-    'cifar10': 10,
-    'cifar100':100,
-    'cinic': 10,
-    'sicapv2': 4,
-    'radboud': 4,
-    'karolinska': 2
-}
-
-classes_per_client = {
-    'cifar10': 10,
-    'cifar100':100,
-    'cinic': 10,
-    'sicapv2': 4,
-    'radboud': 4,
-    'karolinska': 2
-}
-
-args.num_clients = len(args.data_name)
-
-exp_name = f'pFedGP-Full_{args.data_name}_num_clients_{args.num_clients}_seed_{args.seed}_' \
-           f'lr_{args.lr}_num_steps_{args.num_steps}_inner_steps_{args.inner_steps}_' \
-           f'_objective_{args.objective}_predict_ratio_{args.predict_ratio}'
-
-if args.exp_name != '':
-    exp_name += '_' + args.exp_name
-
-logging.info(str(args))
-args.out_dir = (Path(args.save_path) / exp_name).as_posix()
-out_dir = save_experiment(args, None, return_out_dir=True, save_results=False)
-logging.info(out_dir)
-
-def get_optimizer(network):
-    return torch.optim.SGD(network.parameters(), lr=args.lr, weight_decay=args.wd, momentum=0.9) \
-        if args.optimizer == 'sgd' else torch.optim.Adam(network.parameters(), lr=args.lr, weight_decay=args.wd)
-
-
-clients = RealClients(args.data_name, args.data_path, args.num_clients,
-                      batch_size=args.batch_size, input_size=args.input_size, mini=args.mini)
-
-
-# Feds = []
-# for data in args.data_name:
-#     local_model = get_feature_extractor(ft=args.ft, input_size=args.input_size, embedding_dim=num_classes[data], pretrained=False)
-#     local_model = local_model.to(device)
-#     Feds.append(local_model)
-
-
-# optimizers = [get_optimizer(Feds[client_id]) for client_id in range(args.num_clients)]
-
-
-criteria = torch.nn.CrossEntropyLoss()
-results = defaultdict(list)
-
-
-first_name = args.data_name[0]
-net = get_feature_extractor(args.ft, input_size=args.input_size, embedding_dim=num_classes[first_name])
 if cuda:
     print('Cuda is available and used!!!')
     net = net.cuda()
-optimizer = get_optimizer(net)
-print("start training time:", ctime(time()))
-
 
 for epoch in range(10):
     train_loss, val_loss = 0.0, 0.0
-    for i, data in enumerate(clients.train_loaders[0], 0):
+    for i, data in enumerate(train_loader, 0):
         inputs, labels = data
         if cuda:
             inputs, labels = inputs.cuda(), labels.cuda()
         optimizer.zero_grad()
         outputs = net(inputs)
-        loss = criteria(outputs, labels)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
         running_loss = loss.item()
         train_loss += running_loss
         print(f'[{epoch + 1}, {i + 1}] loss: {running_loss:.4f}')
-    train_loss = train_loss / len(clients.train_loaders[0])
+    train_loss = train_loss / len(train_loader)
     correct = 0
     total = 0
     with torch.no_grad():
-        for data in clients.val_loaders[0]:
+        for data in val_loader:
             images, labels = data
             if cuda:
                 images, labels = images.cuda(), labels.cuda()
             outputs = net(images)
-            loss = criteria(outputs, labels)
+            loss = criterion(outputs, labels)
             val_loss += loss.item()
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    val_loss = val_loss / len(clients.val_loaders[0])
-    print(f'{epoch + 1}, Train Loss: {train_loss}, Val Loss: {val_loss}, Acc: {(correct/total)}')
+    val_loss = val_loss / len(val_loader)
+    print(f'{epoch + 1}, Train Loss: {train_loss}, Val Loss: {val_loss}, Acc: {(correct / total)}')
 
 print('Finished Training')
-
