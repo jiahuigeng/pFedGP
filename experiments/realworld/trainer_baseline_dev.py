4c9325b97cc51986576d82290e9652fa1d1207a6
jiahuigeng <jiahui.geng@rwth-aachen.de>
2022-07-13 16:23:42 +0200

    debug baseline

--- experiments/realworld/trainer_baseline_dev.py
+++ experiments/realworld/trainer_baseline_dev.py
@@ -31,7 +31,7 @@ parser.add_argument("--data-path", type=str, default="../datafolder", help="dir 
 parser.add_argument("--ft", '-ft', default='cnn',
                     choices=['cnn', 'resnet18', 'resnet50', 'efficientnetb3', 'efficientnetb5'])
 parser.add_argument("--num-steps", type=int, default=1000)
-parser.add_argument("--input-size", type=int, default=32, help="input size")
+parser.add_argument("--input-size", type=int, default=512, help="input size")
 parser.add_argument("--mini", type=bool, default=True, help="use mini size")
 parser.add_argument("--batch-size", type=int, default=512)
 args = parser.parse_args()
@@ -105,60 +105,61 @@ fix_all_seeds(2021)
 # test_loader = DataLoader(train_set, batch_size=16, shuffle=False)
 
 
-# class ResNet(nn.Module):
-#     def __init__(self, num_channel=3, num_class=4, pretrained=True, model='resnet18'):
-#         super(ResNet, self).__init__()
-#         self.num_channel = num_channel
-#         self.num_class = num_class
-#         self.model = model
-#         self.pretrained = pretrained
-#         if self.model == 'resnet18':
-#             base = resnet.resnet18(pretrained=self.pretrained)
-#             self.resnet_expansion = 1
-#             print("ResNet18 is used.")
-#         elif self.model == 'resnet34':
-#             base = resnet.resnet34(pretrained=self.pretrained)
-#             self.resnet_expansion = 1
-#             print("ResNet34 is used.")
-#         elif self.model == 'resnet50':
-#             base = resnet.resnet50(pretrained=self.pretrained)
-#             self.resnet_expansion = 4
-#             print("ResNet50 is used.")
-#         elif self.model == 'resnet101':
-#             base = resnet.resnet101(pretrained=self.pretrained)
-#             self.resnet_expansion = 4
-#             print("ResNet101 is used.")
-#         elif self.model == 'resnet152':
-#             base = resnet.resnet152(pretrained=self.pretrained)
-#             self.resnet_expansion = 4
-#             print("ResNet152 is used.")
-#         else:
-#             raise NotImplemented('Requested model is not supported.')
-#
-#         self.in_block = nn.Sequential(
-#             nn.Conv2d(self.num_channel, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
-#             base.bn1,
-#             base.relu,
-#             base.maxpool)
-#         self.encoder1 = base.layer1
-#         self.encoder2 = base.layer2
-#         self.encoder3 = base.layer3
-#         self.encoder4 = base.layer4
-#         self.avgpool = base.avgpool
-#         self.flatten = nn.Flatten()
-#         self.fc = nn.Linear(512 * self.resnet_expansion, self.num_class, bias=True)
-#
-#     def forward(self, x):
-#         h = self.in_block(x)
-#         h = self.encoder1(h)
-#         h = self.encoder2(h)
-#         h = self.encoder3(h)
-#         h = self.encoder4(h)
-#         y = self.fc(self.flatten(self.avgpool(h)))
-#         return y
+class ResNet(nn.Module):
+    def __init__(self, num_channel=3, num_class=4, pretrained=True, model='resnet18'):
+        super(ResNet, self).__init__()
+        self.num_channel = num_channel
+        self.num_class = num_class
+        self.model = model
+        self.pretrained = pretrained
+        if self.model == 'resnet18':
+            base = resnet.resnet18(pretrained=self.pretrained)
+            self.resnet_expansion = 1
+            print("ResNet18 is used.")
+        elif self.model == 'resnet34':
+            base = resnet.resnet34(pretrained=self.pretrained)
+            self.resnet_expansion = 1
+            print("ResNet34 is used.")
+        elif self.model == 'resnet50':
+            base = resnet.resnet50(pretrained=self.pretrained)
+            self.resnet_expansion = 4
+            print("ResNet50 is used.")
+        elif self.model == 'resnet101':
+            base = resnet.resnet101(pretrained=self.pretrained)
+            self.resnet_expansion = 4
+            print("ResNet101 is used.")
+        elif self.model == 'resnet152':
+            base = resnet.resnet152(pretrained=self.pretrained)
+            self.resnet_expansion = 4
+            print("ResNet152 is used.")
+        else:
+            raise NotImplemented('Requested model is not supported.')
+
+        self.in_block = nn.Sequential(
+            nn.Conv2d(self.num_channel, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
+            base.bn1,
+            base.relu,
+            base.maxpool)
+        self.encoder1 = base.layer1
+        self.encoder2 = base.layer2
+        self.encoder3 = base.layer3
+        self.encoder4 = base.layer4
+        self.avgpool = base.avgpool
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(512 * self.resnet_expansion, self.num_class, bias=True)
+
+    def forward(self, x):
+        h = self.in_block(x)
+        h = self.encoder1(h)
+        h = self.encoder2(h)
+        h = self.encoder3(h)
+        h = self.encoder4(h)
+        y = self.fc(self.flatten(self.avgpool(h)))
+        return y
 
 data = args.data_name[0]
 net = get_feature_extractor(ft=args.ft, input_size=args.input_size, embedding_dim=num_classes[data], pretrained=True)
+# net = ResNet()
 clients = RealClients(args.data_name, args.data_path, args.num_clients,
                       batch_size=args.batch_size, input_size=args.input_size, mini=args.mini)
 
